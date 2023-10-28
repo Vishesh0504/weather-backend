12daaf5ae6d86d65452d6cffa95119fef80c312f
Vishesh0504 <visheshdpanchal@gmail.com>
2023-10-28 18:23:26 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,36 @@
+const mongoose=require("mongoose");
+let dataSchema=new mongoose.Schema({
+    'temp':{
+        required:true,
+        type:Number
+
+    },
+    'humidity':{
+        required:true,
+        type:Number
+
+    },
+    'pressure':{
+        required:true,
+        type:Number
+
+    },
+    'altitude':{
+        required:true,
+        type:Number
+    },
+    'fl':{
+        required:true,
+        type:Number
+    },
+    'dp':{
+        required:true,
+        type:Number
+    },
+    'time':{
+        required:true,
+        type: Date
+    }
+}
+);
+module.exports =mongoose.model("node_js",dataSchema)
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Data = require("./data"); // Import the Product model
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+const cors = require("cors");
+app.use(cors());
+// Connect to MongoDB using async/await
+(async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect("mongodb+srv://vishesh:050403@cluster0.du9nxq1.mongodb.net/vishesh", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB");
+
+        // Define the /api/add_product route for adding products
+        app.post("/data", async (req, res) => {
+            console.log("Result", req.body);
+
+            // Create a new instance of the Product model using the dataSchema
+            const data = new Data({
+                temp: req.body.temp,
+                humidity: req.body.humidity,
+                pressure: req.body.pressure,
+                altitude: req.body.altitude,
+                fl:req.body.feelsLike,
+                dp:req.body.dewPoint,
+                time: new Date()
+            });
+            console.log(data);
+            try {
+                // Save the product data to the database
+                const dataToStore = await data.save();
+                res.status(200).json(dataToStore);
+            } catch (error) {
+                res.status(400).json({
+                    'status': error.message
+                });
+            }
+        });
+        app.get("/api/get_data", async (req, res) => {
+            try {
+                // Use the `Data` model to find all products in the database
+                const data = await Data.find().sort({ time: -1 }).limit(500); // Use Data.find() to retrieve all products
+
+                res.status(200).json(data);
+            } catch (error) {
+                res.status(500).json({
+                    'status': error.message
+                });
+            }
+        });
+        // ...
+
+        // Route to get the most recent sensor data
+        app.get('/api/mostRecentSensorData', async (req, res) => {
+        try {
+            const mostRecentData = await Data.find().sort({ time: -1 }).limit(1);
+            res.json(mostRecentData);
+        } catch (error) {
+            console.error('Error fetching most recent sensor data:', error);
+            res.status(500).send('Internal Server Error');
+        }
+        });
+
+
+        // Start your Express server
+        app.listen(5000, () => {
+            console.log("Connected to server at 5000");
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+})();
