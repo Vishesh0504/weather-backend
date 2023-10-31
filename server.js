133ced64120261e01c8bbb44a4869bc9057c37e5
Vishesh0504 <visheshdpanchal@gmail.com>
2023-10-31 18:12:01 +0530

    fixed api call

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(cors());
         app.get("/api/get_data", async (req, res) => {
             try {
                 // Use the `Data` model to find all products in the database
-                const data = await Data.find().sort({ "time": -1 }).limit(240); // Use Data.find() to retrieve all products
+                const data = await Data.find().sort({ "timeStamp": -1 }).limit(240); // Use Data.find() to retrieve all products
 
                 res.status(200).json(data);
             } catch (error) {
@@ -79,7 +79,7 @@ app.use(cors());
         // Route to get the most recent sensor data
         app.get('/api/mostRecentSensorData', async (req, res) => {
         try {
-            const mostRecentData = await Data.find().sort({ "time": -1 }).limit(1);
+            const mostRecentData = await Data.find().sort({ "timeStamp": -1 }).limit(1);
             res.json(mostRecentData);
         } catch (error) {
             console.error('Error fetching most recent sensor data:', error);
