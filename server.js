8d254208479a85467d85b1ba01e1939779f60556
Vishesh0504 <visheshdpanchal@gmail.com>
2023-10-31 21:49:02 +0530

    troubleshooting

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Data = require("./data"); // Import the Data model
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
